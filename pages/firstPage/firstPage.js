41c4e3512902ce484a94c810e9c7c8ddcb4079fb
didimali <1615168893@qq.com>
2022-02-18 18:10:08 +0800

    firstpage finished start resolvplan

--- pages/firstPage/firstPage.js
+++ pages/firstPage/firstPage.js
@@ -13,8 +13,11 @@ Page({
     circular: true,
     imgUrls: [
       'https://s4.ax1x.com/2022/02/11/HaV1zT.jpg',
-      'https://s4.ax1x.com/2022/02/11/HaepCt.jpg',
-      'https://s4.ax1x.com/2022/02/11/HaVo6S.jpg'
+      'https://s4.ax1x.com/2022/02/11/HaV1zT.jpg',
+      'https://s4.ax1x.com/2022/02/11/HaV1zT.jpg',
+
+      // 'https://s4.ax1x.com/2022/02/11/HaepCt.jpg',
+      // 'https://s4.ax1x.com/2022/02/11/HaVo6S.jpg'
     ],
     links: [
       '../product/product',
@@ -23,33 +26,49 @@ Page({
     ],
     scrollHeight: "",
     productList: [
-       [{ "name": "1sss", "value": "value1" },
-      { "name": "2sss", "value": "value2" },
-      { "name": "3sss", "value": "value3" },
-      { "name": "4ssss", "value": "value4" }]
-    , 
-      [{ "name": "pagesss", "value": "value1" },
-      { "name": "page2sss", "value": "value2" },
-      { "name": "page3sss", "value": "value3" },
-      { "name": "page4ssss", "value": "value4" }]
+      [{ "name": "银企直连", "value": "提升财务工作效率","id":"1" },
+      { "name": "跨行资金管理平台", "value": "集团企业资金管理","id":"2" },
+      { "name": "结算中心", "value": "总部对成员单位资金统一监控和管理","id":"3" },
+      { "name": "企业网上银行", "value": "全面的网上公司","id":"4" }]
+      ,
+      [{ "name": "企业网上银行", "value": "全面的网上公司金融服务平台","id":"5" },
+      { "name": "单证通", "value": "集团企业资金管理","id":"6" },
+      { "name": "结算中心", "value": "总部对成员单位资金统一监控和管理","id":"7" },
+      { "name": "企业网上银行", "value": "全面的网上公司金融服务平台","id":"8" }]
     ],
-    planList:[
+    planList: [
       {
-        "imgSrc":"https://s4.ax1x.com/2022/02/11/HaV1zT.jpg",
-        "planTheme":"跨行资金解决方案",
-        "planDescrib":"解决您的跨行资金问题"
+        "imgSrc": "https://s4.ax1x.com/2022/02/11/HaV1zT.jpg",
+        "planTheme": "跨行资金解决方案",
+        "planDescrib": "解决您的跨行资金问题",
+        "id":"1"
       },
       {
-        "imgSrc":"https://s4.ax1x.com/2022/02/11/HaV1zT.jpg",
-        "planTheme":"智慧园区解决方案",
-        "planDescrib":"智慧园区解决方案"
+        "imgSrc": "https://s4.ax1x.com/2022/02/11/HaV1zT.jpg",
+        "planTheme": "智慧园区解决方案",
+        "planDescrib": "智慧园区解决方案",
+        "id":"2"
       },
       {
-        "imgSrc":"https://s4.ax1x.com/2022/02/11/HaV1zT.jpg",
-        "planTheme":"制造业行业解决方案",
-        "planDescrib":"制造业行业解决方案"
+        "imgSrc": "https://s4.ax1x.com/2022/02/11/HaV1zT.jpg",
+        "planTheme": "制造业行业解决方案",
+        "planDescrib": "制造业行业解决方案",
+        "id":"3"
+      },
+      {
+        "imgSrc": "https://s4.ax1x.com/2022/02/11/HaV1zT.jpg",
+        "planTheme": "制造业行业解决方案",
+        "planDescrib": "制造业行业解决方案",
+        "id":"4"
       }
-    ]
+    ],
+    zwHeight:"",
+    user:{
+      name:"小条条",
+      teamId:"367890",
+      comAddress:"上海分行闽兴支行",
+      phoneNum:"13867812389"
+    }
   },
   //轮播图的切换事件
   swiperChange: function (e) {
@@ -65,20 +84,56 @@ Page({
       url: this.data.links[this.data.swiperCurrent]
     })
   },
+  //更多
   viewMore() {
     wx.switchTab({
       url: '../product/product',
     })
   },
+  //电话
+  callTell(){
+    let phone = this.data.user.phoneNum.replace(/[^0-9]+/g,'')
+    wx.makePhoneCall({
+      phoneNumber: phone,
+      success:function(){
+        console.log("come in?");
+      }
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //获取屏幕可用高度
-    let screenHeight = wx.getSystemInfoSync().windowHeight + "px";
+    //获取tabbar高度
+    let tabBarHeight = wx.getStorageSync('tabHeight');
+    //获取屏幕信息
+    let res = wx.getSystemInfoSync();
+    //获取屏幕宽度
+    let windowWidth1 =res.windowWidth;
+    let radio = 750/windowWidth1;
+    //获取屏幕高度
+    let windowHeight1 = res.windowHeight;
+    //底部高度适配iphonex等底部有空白的机型
+    const { screenHeight, safeArea: { bottom } } = res;
+    if (screenHeight && bottom){
+      let safeBottom = screenHeight - bottom;
+      let zwHeight = (tabBarHeight+safeBottom)*radio+"rpx";
+       //获取屏幕可用高度
+      let scrollHeight = (windowHeight1 - tabBarHeight-safeBottom)*radio +"rpx";
+      this.setData({
+        zwHeight: zwHeight,
+        scrollHeight: scrollHeight,
+      })
+    }else{
+  //获取屏幕可用高度
+    let scrollHeight = (windowHeight1 - tabBarHeight)*radio +"rpx";
+    let zwHeight = tabBarHeight*radio+"rpx";
     this.setData({
-      scrollHeight: screenHeight
+      scrollHeight: scrollHeight,
+      zwHeight:zwHeight
     })
+    }
   },
 
   /**
@@ -94,12 +149,23 @@ Page({
   onShow: function () {
     if (typeof this.getTabBar === "function" && this.getTabBar()) {
       this.getTabBar().setData({
-      // 当前页面的 tabBar 索引
-      active: 0
+        // 当前页面的 tabBar 索引
+        active: 0
       })
-     }
-  },
+    };
+   
 
+  },
+// attached(){
+//   var query = wx.createSelectorQuery();
+//   query.select('van-tabbar-item').boundingClientRect()
+//   query.exec(function (res) {
+//     //res就是 所有标签为mjltest的元素的信息 的数组
+//     console.log(res);
+//     //取高度
+//     // console.log(res[0].height);
+//   })
+// },
   /**
    * 生命周期函数--监听页面隐藏
    */
@@ -111,6 +177,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
+
 
   },
 
--- pages/incomResolvePlan/incomResolvePlan.js
+++ pages/incomResolvePlan/incomResolvePlan.js
@@ -5,14 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    scrollHeight: "",
+    resolvePlan:{    
+    "title":"跨行资金解决方案",
+    "preface":"企业面临的问题：合作银行分散、u盾管理繁琐、手工环节多、集中管控难、资金效率低。",
+    "outLine":"功能与特点:",
+    "mainBody":[{"text":"统一的用户体系：系统支持统一的用户体系、权限与审批流程设置"},{"text":"统一的账户管理:基于已对接超过100家银行的强大直连能力，我们可将不同银行的不同架构体系聚合至系统中"},{"text":"统一的支付结算：通过对超过100百家银行银企直连接口的适配以及日处理量可达百万"}]
+  }  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //获取屏幕信息
+    let res = wx.getSystemInfoSync();
+    //获取屏幕宽度
+    let windowWidth1 = res.windowWidth;
+    let radio = 750 / windowWidth1;
+    //获取屏幕高度
+    let windowHeight1 = res.windowHeight;
+    //获取屏幕可用高度
+    let scrollHeight = windowHeight1 * radio + "rpx";
+    this.setData({
+      scrollHeight: scrollHeight,
+    })
   },
 
   /**
